a7265ecca9ce65d6c10628057c7e0814cbdddf10
Nguyen Manh Tuan <obmhenry12345@gmail.com>
2023-03-04 17:22:12 +0700

    Done Form Login/Register

--- app/src/main/java/com/example/gymapp/LoginActivity.java
+++ app/src/main/java/com/example/gymapp/LoginActivity.java
@@ -1,4 +1,24 @@
 package com.example.gymapp;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        TextView btnSignUp = findViewById(R.id.txtSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/gymapp/SignUpActivity.java
+++ app/src/main/java/com/example/gymapp/SignUpActivity.java
@@ -1,4 +1,25 @@
 package com.example.gymapp;
 
-public class SignUpActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+
+        TextView txtAlreadyHave = findViewById(R.id.txtAlreadyHave);
+        txtAlreadyHave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
 }
